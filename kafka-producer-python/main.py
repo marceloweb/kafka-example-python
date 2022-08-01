248daab954f68fd2cb1ecb993bb85da03afebbcd
Marcelo Oliveira <marcelo@marceloweb.info>
2022-08-01 14:00:30 -0300

    - random values

--- kafka-producer-python/main.py
+++ kafka-producer-python/main.py
@@ -3,10 +3,9 @@ import random
 import json
 
 number = random.randint(1,100)
-message = 'test: ' + str(number)
-json_message = {'name':'abc', 'email':'abc@example.com'}
+message = "test: {}".format(number)
+bmessage = bytes(message, 'utf-8')
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
-for _ in range(5):
-    producer.send('NOVA_VENDA', message)
-    producer.flush()
+producer.send('NOVA_VENDA', bmessage)
+producer.flush()
