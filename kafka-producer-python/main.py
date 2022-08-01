05bdd16107de407f635cc98c3c923028675f4ebe
Marcelo Oliveira <marcelo@marceloweb.info>
2022-08-01 09:07:43 -0300

    - increment

--- kafka-producer-python/main.py
+++ kafka-producer-python/main.py
@@ -1,6 +1,12 @@
 from kafka import KafkaProducer
+import random
+import json
+
+number = random.randint(1,100)
+message = 'test: ' + str(number)
+json_message = {'name':'abc', 'email':'abc@example.com'}
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 for _ in range(5):
-    producer.send('NOVA_VENDA',b'0009-- Python')
+    producer.send('NOVA_VENDA', message)
     producer.flush()
