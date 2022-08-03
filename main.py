02b7336033b6d51c3919e948d83959baab5f3596
Marcelo Oliveira <marcelo@marceloweb.info>
2022-08-03 08:57:21 -0300

    - new file

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from kafka.admin import KafkaAdminClient, NewTopic
 
-admin_client = KafkaAdminClient(
-        bootstrap_servers = "localhost:9092",
-        client_id = "test_linux"
-)
+def main():
+  print("Welcome to Kafka Test")
+  return
 
-topic_list = []
-topic_list.append(NewTopic(name = "example_topic", num_partitions = 1, replication_factor = 1))
-admin_client.create_topics(new_topics = topic_list, validate_only = False)
+
+main()
