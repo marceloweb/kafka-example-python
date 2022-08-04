551bb5ce4433bd94f9a3097a3dca3f02153a5d25
Marcelo Oliveira <marcelo@marceloweb.info>
2022-08-04 09:25:05 -0300

    - improvements

---
+++ producer/main.py
@@ -0,0 +1,13 @@
+from kafka import KafkaProducer
+import random
+from json import dumps
+
+number = random.randint(100,1000)
+message = "TEST::::::: >> {} <<".format(number)
+bmessage = {'message' :message}
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092', 
+                        value_serializer=lambda x: 
+                         dumps(x).encode('utf-8'))
+producer.send('NOVA_VENDA', value=bmessage)
+producer.flush()
