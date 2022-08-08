e57f856abdb364b676c685efb9e8a948f74ce05f
Marcelo Oliveira <marcelo@marceloweb.info>
2022-08-08 07:54:21 -0300

    - added function producer

--- producer/main.py
+++ producer/main.py
@@ -2,12 +2,15 @@ from kafka import KafkaProducer
 import random
 from json import dumps
 
-number = random.randint(100,1000)
-message = "TEST::::::: >> {} <<".format(number)
-bmessage = {'message' :message}
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092', 
-                        value_serializer=lambda x: 
-                         dumps(x).encode('utf-8'))
-producer.send('NOVA_VENDA', value=bmessage)
-producer.flush()
+def producer():
+   number = random.randint(100,1000)
+   message = "TEST::::::: >> {} <<".format(number)
+   bmessage = {'message' :message}
+
+   producer = KafkaProducer(bootstrap_servers='localhost:9092', 
+                           value_serializer=lambda x: 
+                            dumps(x).encode('utf-8'))
+   producer.send('NOVA_VENDA', value=bmessage)
+   producer.flush()
+
