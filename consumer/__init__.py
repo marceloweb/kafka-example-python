f2851ae891d802a828ebaf5e13fd1882ae1a0844
Marcelo Oliveira <marcelo@marceloweb.info>
2022-09-01 10:50:37 -0300

    - new configs

--- consumer/__init__.py
+++ consumer/__init__.py
@@ -2,7 +2,8 @@ from kafka import KafkaConsumer
 import configs
 
 def consumer():
-   conf = configs.load('configs/kafka/kafka.yml') 
-   consumer = KafkaConsumer('NOVA_VENDA', group_id='ECOMMERCE', bootstrap_servers='localhost:9092')
+   conf = configs.load('configs/kafka/kafka.yml')
+   message = 'test'
+   consumer = KafkaConsumer(conf['consumer']['topic'], group_id=conf['consumer']['group_id'], bootstrap_servers=conf['consumer']['host'])
    for msg in consumer:
       print (msg)
