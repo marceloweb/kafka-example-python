90a63572d450f2fda652d6a9f710a98b11340317
Marcelo Oliveira <marcelo@marceloweb.info>
2022-09-01 10:45:30 -0300

    - added config/kafka

--- consumer/__init__.py
+++ consumer/__init__.py
@@ -1,6 +1,8 @@
 from kafka import KafkaConsumer
+import configs
 
 def consumer():
+   conf = configs.load('configs/kafka/kafka.yml') 
    consumer = KafkaConsumer('NOVA_VENDA', group_id='ECOMMERCE', bootstrap_servers='localhost:9092')
    for msg in consumer:
       print (msg)
