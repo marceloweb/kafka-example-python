f72347978937ee6d4368a3e48ca7675f1e29e63c
Marcelo Oliveira <marcelo@marceloweb.info>
2022-09-01 11:30:42 -0300

    - improving the configs

--- producer/__init__.py
+++ producer/__init__.py
@@ -3,8 +3,20 @@ import random
 from json import dumps
 import configs
 
+def configuration():
+   file = configs.load('configs/kafka/kafka.yml')
+   conf = {
+         'bootstrap.servers': file['consumer']['host'],
+         'group.id': "%s-consumer" % file['consumer']['group_id'],
+         'session.timeout.ms': 6000,
+         'default.topic.configs': {'auto.offset.reset': 'smallest'},
+         'security.protocol': 'SASL_SSL',
+         'sasl.mechanisms': 'SCRAM-SHA-256',
+         'sasl.username': file['consumer']['username'],
+         'sasl.password': file['consumer']['password']
+   }
+
 def producer():
-   conf = configs.load('configs/kafka/kafka.yml')
    number = random.randint(100,1000)
    message = "TEST::::::: >> {} <<".format(number)
    bmessage = {'message' :message}
