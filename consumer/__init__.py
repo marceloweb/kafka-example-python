b5b6f5508ef88b6c834d8b128b72229fd947d127
Marcelo Oliveira <marcelo@marceloweb.info>
2022-09-02 19:11:43 -0300

    - new version

--- consumer/__init__.py
+++ consumer/__init__.py
@@ -4,7 +4,7 @@ import configs
 def configuration():
    file = configs.load('configs/kafka/kafka.yml')
    conf = {
-         'bootstrap.servers': file['consumer']['host'],
+         'bootstrap.servers': str(file['consumer']['host']),
          'group.id': "%s-consumer" % file['consumer']['group_id'],
          'session.timeout.ms': 6000,
          'default.topic.configs': {'auto.offset.reset': 'smallest'},
@@ -17,6 +17,6 @@ def configuration():
 def consumer():
    conf = configs.load('configs/kafka/kafka.yml')
    message = 'test'
-   consumer = KafkaConsumer(conf['consumer']['topic'], group_id=conf['consumer']['group_id'], bootstrap_servers=conf['consumer']['host'])
+   consumer = KafkaConsumer(conf['consumer']['topic'], group_id=conf['consumer']['group_id'], bootstrap_servers=str(conf['consumer']['host']))
    for msg in consumer:
       print (msg)
--- producer/__init__.py
+++ producer/__init__.py
@@ -1,29 +1,45 @@
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 import random
+import sys
 from json import dumps
 import configs
-
-def configuration():
-   file = configs.load('configs/kafka/kafka.yml')
-   conf = {
-         'bootstrap.servers': file['consumer']['host'],
-         'group.id': "%s-consumer" % file['consumer']['group_id'],
-         'session.timeout.ms': 6000,
-         'default.topic.configs': {'auto.offset.reset': 'smallest'},
-         'security.protocol': 'SASL_SSL',
-         'sasl.mechanisms': 'SCRAM-SHA-256',
-         'sasl.username': file['consumer']['username'],
-         'sasl.password': file['consumer']['password']
-   }
+from confluent_kafka import SerializingProducer
+from confluent_kafka.serialization import StringSerializer
 
 def producer():
+   file = configs.load('configs/kafka/kafka.yml')
    number = random.randint(100,1000)
    message = "TEST::::::: >> {} <<".format(number)
    bmessage = {'message' :message}
 
-   producer = KafkaProducer(bootstrap_servers=conf['producer']['host'], 
-                           value_serializer=lambda x: 
-                            dumps(x).encode('utf-8'))
-   producer.send(conf['producer']['topic'], value=bmessage)
-   producer.flush()
+   conf = {
+        'bootstrap.servers': "",
+        'session.timeout.ms': 6000,
+        'value.serializer': StringSerializer('utf_8'),
+        'key.serializer': StringSerializer('utf_8'),
+        'default.topic.config': {'auto.offset.reset': 'smallest'},
+        'security.protocol': 'SASL_SSL',
+	    'sasl.mechanisms': 'SCRAM-SHA-256',
+        'sasl.username': file['producer']['username'],
+        'sasl.password': file['producer']['password']
+    }
+
+   p = SerializingProducer(conf)
+
+   def delivery_callback(err, msg):
+        if err:
+            sys.stderr.write('%% Message failed delivery: %s\n' % err)
+        else:
+            sys.stderr.write('%% Message delivered to %s [%d]\n' %
+                             (msg.topic(), msg.partition()))
+
+   try:
+      p.produce(file['producer']['topic'], key="name", value="Marcx")
+   except BufferError as e:
+            sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
+                             len(p))
+   p.poll(0)
+
+   sys.stderr.write('%% Waiting for %d deliveries\n' % len(p))
+   p.flush()
 
